78c53e818e1f851d0dedc9e81802d33573d17365
Seyanokeng <seyanokenglentswana@gmail.com>
2021-07-12 13:14:15 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
@@ -10,7 +9,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: 'left',
     color: theme.palette.text.secondary,
   },
 }));
@@ -20,30 +19,76 @@ export default function CenteredGrid() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <h1>curriculum vitae of Seyanokeng Lentswana</h1>
+      <Grid container spacing={0}>
+      <Grid item xs={4}>
+          <Paper className={classes.paper}>picture
+          
+          
+          
+          jnjhnjyhnbtyj jyhnbj nyhfffffffffh n ghn hnbhyhy yh bhbbhbyh
           </Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>profile
+          tgvbhbhjnjfjvgsvgvhtbhbfhhghhbxhhcbhcfb hg bgbgv gv 
+
+          gfvgfvvfsetsgetgtstsgtrrtdtgffhtgfrthtrhrthrtdgrtgrgstd
+          rthythdythy6wy5w4yw54yyw64w5wtw5yw5456y4t45t45t45t4ttftft
+          vtfftefte
+          </Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Name</Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>contact
+          
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          
+          
+          
+          </Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>Experience
+          
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          hybhbybhybgbvhvbgvhbhfhhbhyfbfyhbjhnukikmimkmjuhbyvgt
+          
+          
+          
+          </Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
+  
       </Grid>
     </div>
   );
